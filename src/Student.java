9da89899ded340d7a0fa7202fa59559f19deb33d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2021-11-16 12:18:18 +0000

    Second Commit: Completed Student Class

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,56 @@
 /**
- * Student Class: This class keeps track of the student's name, grades and status of tuition fees.
+ * Created by Adrian Adewunmi on 16/11/21.
+ * Version 1.0
+ * Student Class: This class keeps track of the student's name, ID and status of tuition fees.
  */
-public class Student {
+
+class Student {
+    private int id;
+    private String name;
+    private String courseName;
+    private String courseCode;
+    private double tuitionFees;
+    private double amountPaidTuitionFees;
+
+    public Student(int id, String name, String courseName, String courseCode, double tuitionFees) {
+        this.id = id;
+        this.name = name;
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.tuitionFees = tuitionFees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public double getTuitionFees() {
+        return tuitionFees;
+    }
+
+    public void setAmountPaidTuitionFees(double amountPaidTuitionFees) {
+        this.amountPaidTuitionFees = amountPaidTuitionFees;
+    }
+
+    public double getAmountPaidTuitionFees() {
+        return amountPaidTuitionFees;
+    }
+
+    public double updateTuitionFees(){
+        tuitionFees = getTuitionFees();
+        amountPaidTuitionFees = getAmountPaidTuitionFees();
+        return tuitionFees = tuitionFees - amountPaidTuitionFees;
+    }
 }
