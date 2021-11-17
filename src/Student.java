03ec27b792743318b1d2477e1aaff7ed38f55b66
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2021-11-17 12:06:16 +0000

    Fifth Commit: Completed Teacher Class

--- src/Student.java
+++ src/Student.java
@@ -6,12 +6,21 @@
  */
 
 class Student {
-    private int id;
-    private String name;
-    private String courseName;
-    private String courseCode;
+    private final int id;
+    private final String name;
+    private final String courseName;
+    private final String courseCode;
     private double tuitionFees;
     private double amountPaidTuitionFees;
+
+    /**
+     * Constructor for Student class
+     * @param id: Student's ID
+     * @param name: Student's name
+     * @param courseName: Student's course name
+     * @param courseCode: Student's course code
+     * @param tuitionFees: Student's tuition fees
+     */
 
     public Student(int id, String name, String courseName, String courseCode, double tuitionFees) {
         this.id = id;
@@ -21,34 +30,58 @@ class Student {
         this.tuitionFees = tuitionFees;
     }
 
+    /**
+     * This method returns the student's ID
+     * @return: Student's ID
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * This method returns the student's name
+     * @return: Student's name
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * This method returns the student's course name
+     * @return: Student's course name
+     */
     public String getCourseName() {
         return courseName;
     }
-
+    /**
+     * This method returns the student's course code
+     * @return: Student's course code
+     */
     public String getCourseCode() {
         return courseCode;
     }
-
+    /**
+     * This method returns the student's tuition fees
+     * @return: Student's tuition fees
+     */
     public double getTuitionFees() {
         return tuitionFees;
     }
-
+    /**
+     * This method sets the tuition fees amount paid by student
+     */
     public void setAmountPaidTuitionFees(double amountPaidTuitionFees) {
         this.amountPaidTuitionFees = amountPaidTuitionFees;
     }
-
+    /**
+     * This method the balance of tuition fees amount paid by student
+     * @return: Student's amount paid tuition fees
+     */
     public double getAmountPaidTuitionFees() {
         return amountPaidTuitionFees;
     }
-
+    /**
+     * This method returns the student's balance tuition fees
+     * @return: Student's balance tuition fees
+     */
     public void updateTuitionFees(){
         try {
             tuitionFees = getTuitionFees();
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,53 @@
+
 /**
  * Created by Adrian Adewunmi on 17/11/21.
  * Version 1.0
- * Teacher Class:
+ * Teacher Class: This class keeps track of the teacher's name, id, and salary.
  */
 
 public class Teacher {
+    private final int id;
+    private final String name;
+    private String salary;
+
+    /**
+     * Constructor for the Teacher class
+     * @param id: The teacher's id
+     * @param name: The teacher's name
+     * @param salary: The teacher's salary
+     */
+
+    public Teacher(int id, String name, String salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    /**
+     * Gets the teacher's id
+     * @return: The teacher's id
+     */
+    public int getId() {
+        return id;
+    }
+    /**
+     * Gets the teacher's name
+     * @return: The teacher's name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Gets the teacher's salary
+     * @return: The teacher's salary
+     */
+    public String getSalary() {
+        return salary;
+    }
+    /**
+     * Sets the teacher's salary
+     * @param salary: The teacher's salary
+     */
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
 }
