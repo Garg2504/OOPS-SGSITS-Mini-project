c5ba6e27520fd26105d85807c4d721b31453ea0c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2021-11-28 11:54:39 +0000

    Eleventh Commit: Implemented addTeachers and printAllTeachers methods in MainClass.

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,8 +10,10 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         int selectMainMenu;
+        int selectSubMenu;
         College newCollege = new College("New College");
-        System.out.println("===================================\n" + newCollege.getCollegeName() + " Financial System"
+        System.out.println("===================================\n" + newCollege.getCollegeName()
+                + " Financial System"
                 + " (NCFS)" + "\n===================================");
         LocalDate date = LocalDate.now();
         System.out.println("Date: " + date + "\n");
@@ -27,6 +29,37 @@ public class MainClass {
             switch (selectMainMenu){
                 case 1:
                     System.out.println("\n=== Teachers Finance ===");
+                    do{
+                        System.out.println("1. Add Teacher\n" +
+                                "2. Find Teacher\n" +
+                                "3. Increase Salary\n" +
+                                "4. Delete Teacher\n" +
+                                "5. Print All Teachers\n" +
+                                "6. Exit");
+                        System.out.print("\nSelect an option: ");
+                        Scanner scannerSubMenu = new Scanner(System.in);
+                        selectSubMenu = scannerSubMenu.nextInt();
+                        switch (selectSubMenu){
+                            case 1:
+                                addTeachers(newCollege);
+                                break;
+                            case 2:
+                                System.out.println("\n=== Find Teacher ===\n");
+                                break;
+                            case 3:
+                                System.out.println("\n=== Increase Salary ===\n");
+                                break;
+                            case 4:
+                                System.out.println("\n=== Delete Teacher ===\n");
+                                break;
+                            case 5:
+                                printAllTeachers(newCollege);
+                                break;
+                            case 6:
+                                System.out.println("\n=== Exit ===");
+                                break;
+                        }
+                    }while(selectSubMenu != 6);
                     break;
                 case 2:
                     System.out.println("\n=== Students Finance ===");
@@ -36,7 +69,34 @@ public class MainClass {
                     break;
                 case 4:
                     System.out.println("\nThank you for using NCFS");
+                    break;
             }
         }while(selectMainMenu != 4);
+    }
+
+    public static void addTeachers(College newCollege){
+        char y = 'y';
+        System.out.println("=== Add Teacher ===");
+        do{
+            System.out.print("\nEnter teacher id: ");
+            Scanner scanner = new Scanner(System.in);
+            String teacherID = scanner.nextLine();
+            System.out.print("Enter the name of the teacher: ");
+            scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            System.out.print("Enter the salary of the teacher: ");
+            double salary = scanner.nextDouble();
+            Teacher newTeacher = new Teacher(teacherID, name, salary);
+            newCollege.addTeacher(newTeacher);
+            System.out.print("\nAdd another teacher? (y/n): \n");
+            scanner = new Scanner(System.in);
+            y = scanner.next().charAt(0);
+        } while(y == 'y');
+    }
+
+    public static void printAllTeachers(College newCollege){
+        System.out.println("\n=== Teachers ===");
+        newCollege.printListOfTeachers();
+        System.out.println("\n");
     }
 }
